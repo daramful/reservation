84c4d876716b456ce3177a4f54065bf855f184a2
dykang2 <junbin.park@gmail.com>
2018-02-25 17:50:34 -0600

    foundation integrated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-var fs = require('fs');
-
+const express = require("express");
+// var fs = require('fs');
 var app = express();
 app.use(express.static(__dirname + "/static"));
 app.listen(port, hostname);
