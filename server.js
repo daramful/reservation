739215424a945dd59f521f7654e5d8dd649ed0c1
Ginny <helee295@gmail.com>
2018-02-25 17:56:23 -0600

    scss installed

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+/*
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+var port = process.env.PORT || 3000;
 var fs = require('fs');
+
+
+var connect = require('connect');
+var sassMiddleware = require('node-sass-middleware');
+
+var srcPath = __dirname + '/public/sass';
+var destPath = __dirname + '/public/styles';
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +19,30 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at ${hostname}:${port}/`);
 });
+*/
+
+var connect = require('connect');
+var sassMiddleware = require('node-sass-middleware');
+
+var srcPath = __dirname + '/public/sass';
+var destPath = __dirname + '/public/styles';
+
+var serveStatic = require('serve-static')
+var http = require('http');
+var port = process.env.PORT || 8000;
+var app = connect();
+app.use('/styles', sassMiddleware({
+  src: srcPath,
+  dest: destPath,
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use('/',
+  serveStatic('./public', {})
+);
+http.createServer(app).listen(port);
+console.log('Server listening on port ' + port);
+console.log('srcPath is ' + srcPath);
+console.log('destPath is ' + destPath);
