7b8654fe729fa0ac35e1efe8439118dde54e9c34
Ginny <helee295@gmail.com>
2018-02-25 18:42:53 -0600

    moved foundation_sample/lib to lib & set index.html from public

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-var port = process.env.PORT || 8000;
+var port = 3000;
+
 const express = require("express");
-
-var connect = require('connect');
 var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+var app = express();
 
-var srcPath = __dirname + '/public/sass';
-var destPath = __dirname + '/public/styles';
+const srcPath = __dirname + '/public/sass';
+const destPath =  __dirname + '/public/styles';
 
-var serveStatic = require('serve-static')
-var app = connect();
-app.use('/styles', sassMiddleware({
+app.use(sassMiddleware({
   src: srcPath,
   dest: destPath,
   debug: true,
-  outputStyle: 'expanded'
+  outputStyle: 'extended',
+  prefix: '/styles'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/> 
 }));
-app.use('/',
-  serveStatic('./public', {})
-);
 
-var app = express();
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/public"));
 app.listen(port, hostname);
+
+
+console.log('Server listening on port ' + port);
+console.log('srcPath is ' + srcPath);
+console.log('destPath is ' + destPath);
